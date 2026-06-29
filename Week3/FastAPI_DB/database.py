d090a80613e7fb0c4d77d70266ee2743a1af6a3d
Ramya Dhamodharan <ramyadhamodharan2005@gmail.com>
2026-06-29 16:06:33 +0530

    week3

--- Week3/FastAPI_DB/database.py
+++ Week3/FastAPI_DB/database.py
@@ -3,12 +3,12 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
 
-# .env file load பண்ணு — இது FIRST இருக்கணும்!
+
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Debug — None வருதான்னு check பண்ண
+
 print(f"DATABASE_URL = {DATABASE_URL}")
 
 engine = create_engine(DATABASE_URL)
